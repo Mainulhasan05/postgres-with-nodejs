88ac868a3eb1f89f6c6f69771e5cea852bccdf4d
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-11-19 23:12:00 +0600

    initial db connection

---
+++ config/db.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false, // Set true for SQL logs
+  }
+);
+
+const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection to PostgreSQL has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error.message);
+  }
+};
+
+testConnection();
+
+module.exports = sequelize;
